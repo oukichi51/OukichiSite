ab515ce49420acf88fb97b760a5352900d265ae4
oukichi51 <oukichi51@gmail.com>
2024-12-22 19:05:41 +0900

    Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'], // src内のHTMLとJSファイルを監視
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+      },
+    },
+  },
+  plugins: [],
+};
